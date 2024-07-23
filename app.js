eb61ed8a91306f48cb647cb86899e32c2665a909
Colombiantechnology <1107050440f@gmail.com>
2024-07-23 12:05:14 -0500

    Bonito ejemplo de geo

---
+++ app.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+const http = require("http");
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = process.env.PORT || "3000";
+app.set("port", port);
+
+/**
+ * Create HTTP server.
+ */
+
+io.on("connection", (socket) => {
+  socket.on("setCoordenadas", (data) => {
+    socket.emit("setCoordenadas", data);
+  });
+  console.log(socket.id);
+});
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port, () => {
+  console.log(`corriendo en Puerto ${port}`);
+});
